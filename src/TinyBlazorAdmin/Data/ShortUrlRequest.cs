60cdb3c1e45eace45450b39cdfbbe3fd51a4c388
Frank <fboucher@outlook.com>
2022-04-29 21:32:19 +0000

    still broken

--- src/TinyBlazorAdmin/Data/ShortUrlRequest.cs
+++ src/TinyBlazorAdmin/Data/ShortUrlRequest.cs
@@ -1,42 +1,42 @@
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using Cloud5mins.AzShortener;
+// using System.Collections.Generic;
+// using System.ComponentModel.DataAnnotations;
+// using Cloud5mins.AzShortener;
 
-namespace Cloud5mins.TinyBlazorAdmin.Data
-{
-    public class ShortUrlRequest
-    {
-        private string _vanity;
+// namespace Cloud5mins.TinyBlazorAdmin.Data
+// {
+//     public class ShortUrlRequest
+//     {
+//         private string _vanity;
 
-        public string Title { get; set; }
+//         public string Title { get; set; }
 
-        public string Vanity
-        {
-            get
-            {
-                return (_vanity != null) ? _vanity : string.Empty;
-            }
-            set
-            {
-                _vanity = value;
-            }
-        }
+//         public string Vanity
+//         {
+//             get
+//             {
+//                 return (_vanity != null) ? _vanity : string.Empty;
+//             }
+//             set
+//             {
+//                 _vanity = value;
+//             }
+//         }
 
-        [Required]
-        public string Url { get; set; }
+//         [Required]
+//         public string Url { get; set; }
 
-        private List<Schedule> _schedules;
+//         private List<Schedule> _schedules;
 
-        public List<Schedule> Schedules { 
-            get{
-                if(_schedules == null){
-                    _schedules = new List<Schedule>();
-                }
-                return _schedules;
-            } 
-            set{
-                _schedules = value;
-            } 
-        }
-    }
-}
+//         public List<Schedule> Schedules { 
+//             get{
+//                 if(_schedules == null){
+//                     _schedules = new List<Schedule>();
+//                 }
+//                 return _schedules;
+//             } 
+//             set{
+//                 _schedules = value;
+//             } 
+//         }
+//     }
+// }
