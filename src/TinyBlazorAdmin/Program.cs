94e7843e2032a9853d8bdd7204e71d05c8d004d7
fboucher <fboucher@outlook.com>
2022-04-29 01:10:50 +0000

    work in progress

--- src/TinyBlazorAdmin/Program.cs
+++ src/TinyBlazorAdmin/Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Cloud5mins.TinyBlazorAdmin;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
+// set up a delegate to get function token
+static string functionEndpoint(WebAssemblyHostBuilder builder) =>
+    builder.Configuration
+        .GetSection(nameof(UrlShortenerSecuredService))
+        .GetValue<string>(nameof(AzFuncAuthorizationMessageHandler.Endpoint));
+
+//Add SycnFusion Controls
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzgwNjM5QDMxMzgyZTMzMmUzMGhPbXloTFpmTVFQTEgrMUZ2NjZONkFEZmhLOG16RkhYSkYyZmZpOHRVUkU9"); 
+builder.Services.AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
