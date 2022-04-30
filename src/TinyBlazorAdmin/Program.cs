5f6ee3a7a61d1334acd230a0888be0b326bc9024
Frank <fboucher@outlook.com>
2022-04-30 13:29:21 +0000

    adding SyncFusion grid back :)

--- src/TinyBlazorAdmin/Program.cs
+++ src/TinyBlazorAdmin/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
 using Cloud5mins.TinyBlazorAdmin;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+// regiser fusion blazor service
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
 
 await builder.Build().RunAsync();
